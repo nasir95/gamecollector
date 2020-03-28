00bbc0a5da6b8a3d7008b1ab7491006400cad98c
nasir <nasir@Nasirs-MBP.fios-router.home>
2020-03-28 13:23:04 -0400

    add views

--- gamecollector/main_app/views.py
+++ gamecollector/main_app/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Game
+
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def games_index(request):
+    games = Game.objects.all()
+    return render(request, 'games/index.html', {'games': games})
+
+
+def games_detail(request, cat_id):
+    game = Game.objects.get(id=game_id)
+    return render(request, 'games/detail.html', {'game': game})
+
+
+class GameCreate(CreateView):
+    model = Game
+    fields = '__all__'
+
+
+class GameUpdate(UpdateView):
+    model = Game
+    fields = ['breed', 'description', 'age']
+
+
+class GameDelete(DeleteView):
+    model = Game
+    success_url = '/games/'
