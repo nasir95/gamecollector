b5132b6324d103c65fa109d868e22afcc9ce4a4d
nasir <nasir@Nasirs-MBP.fios-router.home>
2020-03-28 13:30:54 -0400

    added game model

--- gamecollector/main_app/models.py
+++ gamecollector/main_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
+
+
+class Game(models.Model):
+    name = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    rating = models.CharField(max_length=2)
+    description = models.TextField(max_length=250)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'game_id': self.id})
