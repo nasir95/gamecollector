e66ad5980e634e9e91e320a500d7c64fb698cfeb
nasir <nasir@Nasirs-MBP.fios-router.home>
2020-04-22 20:26:55 -0400

    game collector adjustments done

--- gamecollector/main_app/models.py
+++ gamecollector/main_app/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Console(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"A {self.color} {self.name}"
 
 
 class Game(models.Model):
@@ -8,7 +17,9 @@ class Game(models.Model):
     genre = models.CharField(max_length=100)
     rating = models.CharField(max_length=2)
     description = models.TextField(max_length=250)
-    year = models.IntegerField()
+    date = models.DateField('Released Year')
+    consoles = models.ManyToManyField(Console)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
--- gamecollector/main_app/views.py
+++ gamecollector/main_app/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Game
+from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Game, Console
 
 
 # Create your views here.
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
 
 
 def home(request):
@@ -21,13 +41,28 @@ def games_index(request):
 
 def games_detail(request, game_id):
     game = Game.objects.get(id=game_id)
-    return render(request, 'games/detail.html', {'game': game})
+    consoles_game_doesnt_have = Console.objects.exclude(
+        id__in=game.consoles.all().values_list('id'))
+    return render(request, 'games/detail.html', {'game': game, 'consoles': consoles_game_doesnt_have})
+
+
+def assoc_console(request, game_id, console_id):
+    Game.objects.get(id=game_id).consoles.add(console_id)
+    return redirect('detail', game_id=game_id)
+
+
+def remove_assoc_console(request, game_id, console_id):
+    Game.objects.get(id=game_id).consoles.remove(console_id)
+    return redirect('detail', game_id=game_id)
 
 
 class GameCreate(CreateView):
     model = Game
-    fields = '__all__'
-    success_url = '/games/'
+    fields = ['name', 'genre', 'rating', 'description', 'date']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
 
 class GameUpdate(UpdateView):
@@ -38,3 +73,28 @@ class GameUpdate(UpdateView):
 class GameDelete(DeleteView):
     model = Game
     success_url = '/games/'
+
+
+class ConsoleList(ListView):
+    model = Console
+
+
+class ConsoleDetail(DetailView):
+    model = Console
+
+
+class ConsoleCreate(CreateView):
+    model = Console
+    fields = ['name', 'color']
+    success_url = '/consoles/'
+
+
+class ConsoleUpdate(UpdateView):
+    model = Console
+    fields = ['name', 'color']
+    success_url = '/consoles/'
+
+
+class ConsoleDelete(DeleteView):
+    model = Console
+    success_url = '/consoles/'
